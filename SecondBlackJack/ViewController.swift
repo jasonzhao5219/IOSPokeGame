314a0c78af55adaaaafa59cfb00ec80e8fea5894
William_Tsai <tsaiyuhlin@gmail.com>
2018-01-14 22:30:28 -0800

    Added game logic

--- SecondBlackJack/ViewController.swift
+++ SecondBlackJack/ViewController.swift
@@ -2,147 +2,198 @@ import UIKit
 import CoreMotion
 
 class ViewController: UIViewController {
-
-    @IBOutlet var card1: UIImageView!
-    @IBOutlet var card2: UIImageView!
-    @IBOutlet var card3: UIImageView!
-    @IBOutlet var card4: UIImageView!
-    @IBOutlet var card5: UIImageView!
-    @IBOutlet var card6: UIImageView!
-    @IBOutlet var card7: UIImageView!
-    @IBOutlet var card8: UIImageView!
-    @IBOutlet var card9: UIImageView!
-    @IBOutlet var card10: UIImageView!
-
-    @IBOutlet var MyPoints: UILabel!
+    
+    @IBOutlet var cardGroup1: [UIImageView]!
+    
+    @IBOutlet var cardGroup2: [UIImageView]!
+    
+    @IBOutlet var welcomeLabel: UILabel!
     
     @IBOutlet var restartBtn: UIButton!
     @IBAction func restartBtnPressed(_ sender: UIButton) {
-        turnCounter=0
-        MyturnCounter=1
-        YourturnCounter=0
-        welcomeLabel.isHidden = false
-        
-        card1.isHidden = true
-        card2.isHidden = true
-        card3.isHidden = true
-        card4.isHidden = true
-        card5.isHidden = true
-        card6.isHidden = true
-        card7.isHidden = true
-        card8.isHidden = true
-        card9.isHidden = true
-        card10.isHidden = true
-       
-        
+        endMatch = false
+        playerTurn = 1
+        for card in cardGroup1 {
+            card.isHidden = true
+        }
+        for card in cardGroup2 {
+            card.isHidden = true
+        }
     }
     
-    @IBAction func pass2(_ sender: UIButton) {
-        //turnCounter+=1
-        turnCounter=YourturnCounter
-        MyturnCounter+=2
+    @IBOutlet var player2PassBtn: UIButton!
+    @IBAction func player2PassBtnPressed(_ sender: UIButton) {
+        playerTurn += 1
         updateUI()
     }
     
     @IBOutlet var upsideDownBtn: UIButton!
     @IBAction func upsideDownBtnPressed(_ sender: UIButton) {
-        //turnCounter+=1
-        turnCounter=MyturnCounter
-        YourturnCounter+=2
-         updateUI()
-        
+        playerTurn += 1
+        updateUI()
     }
     
-    var randomnum:[Int]=[Int]()
-
-    @IBOutlet var cardImage: UIImageView!
-    
-    @IBOutlet var welcomeLabel: UILabel!
-    
-    @IBAction func displaymypoints(_ sender: UIButton) {
-        //updateUI()
-        
-        
-    }
-
-    @IBOutlet var MessageLabel: UILabel!
-    
-    var turnCounter:Int=0
-    var MyturnCounter:Int=1
-    var YourturnCounter:Int=0
+    var turnCounter: Int = 0
+    var MyturnCounter: Int = 1
+    var YourturnCounter: Int = 0
     //control what my points is and how to calcaulate it:
-    func updateUI() {
-        //black jack game rule here
-        var totalpoints:Int=0
-        
-    
-        totalpoints+=randomnum[randomnum.count-1]
-        
-        print(totalpoints)
-        MessageLabel.text = "my total points is\(totalpoints)"
-        MessageLabel.isHidden = true
-        if totalpoints>21{
-            MessageLabel.text="Bomb!"
-        }
-    }
-    
     var motionManager: CMMotionManager?
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        restartBtn.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 2)
-        upsideDownBtn.transform =
-            CGAffineTransform(rotationAngle: CGFloat.pi)
-        MessageLabel.isHidden = true
-        welcomeLabel.text = "Shake me"
-        card1.isHidden = true
-        card2.isHidden = true
-        card3.isHidden = true
-        card4.isHidden = true
-        card5.isHidden = true
-        card6.isHidden = true
-        card7.isHidden = true
-        card8.isHidden = true
-        card9.isHidden = true
-        card10.isHidden = true
-        
-        //Assigning instance of MotionManager to variable
+    let player1 = Player(name: "Player 1")
+    let player2 = Player(name: "Player 2")
+    var endMatch = false
+    var playerTurn = 1
+    
+    func updateUI() {
         motionManager = CMMotionManager()
-        
-        //Safely unwrapping and accessing manager
         if let manager = motionManager {
-            manager.deviceMotionUpdateInterval=0.2
-            print("We have our manager")
-            
-            //Establish alternate Queueueueue to handle updates
-            let myQ = OperationQueue()
-            
-            //Call method to start updates, sending in myQ and closure to handle data
-            manager.startDeviceMotionUpdates(to: myQ, withHandler: {
-                (data: CMDeviceMotion?, error: Error?) in
-                
-                //Safely unwrapping data or error
-                if let myData = data {
-                    let mynumberBlack=myData.userAcceleration.x
-                    let mynumberRed=myData.userAcceleration.y
-                    if mynumberBlack>1.0 {
-                        DispatchQueue.main.async {
-                            self.dealBlack()
-                            self.turnCounter+=2
-                            if self.turnCounter%2==1
-                            {self.MyturnCounter+=2}
+            manager.deviceMotionUpdateInterval = 0.2
+            let myQueue = OperationQueue()
+            manager.startDeviceMotionUpdates(to: myQueue, withHandler: {(data: CMDeviceMotion?, error: Error?) in
+                var cardSum1 = 0
+                var cardSum2 = 0
+                while self.endMatch == false {
+                    if self.playerTurn % 2 != 0 {
+                        //Player 1's turn
+                        self.welcomeLabel.text = "\(self.player1.playerName)'s turn"
+                        self.welcomeLabel.isHidden = false
+                        if let myData = data {
+                            let myNumberBlack = myData.userAcceleration.x
+                            let myNumberRed = myData.userAcceleration.y
+                            for number in self.player1.hand {
+                                if number == 1 {
+                                    cardSum1 += 11
+                                } else if number > 10 {
+                                    cardSum1 += 10
+                                }
+                                else {
+                                    cardSum1 += number
+                                }
+                            }
+                            if cardSum1 < 22 {
+                                if myNumberBlack > 0.6 {
+                                    DispatchQueue.main.async {
+                                        let newCard = self.dealBlack()
+                                        self.cardGroup1[self.playerTurn].image = UIImage(named: newCard.0)
+                                        self.cardGroup1[self.playerTurn].isHidden = false
+                                        self.player1.hand.append(newCard.1)
+                                    }
+                                } else if myNumberRed > 0.6 {
+                                    DispatchQueue.main.async {
+                                        let newCard = self.dealRed()
+                                        self.cardGroup1[self.playerTurn].image = UIImage(named: newCard.0)
+                                        self.cardGroup1[self.playerTurn].isHidden = false
+                                        self.player1.hand.append(newCard.1)
+                                    }
+                                }
+                            } else if cardSum1 > 21 && self.player1.hand.contains(1) {
+                                cardSum1 -= 10
+                                if myNumberBlack > 0.6 {
+                                    DispatchQueue.main.async {
+                                        let newCard = self.dealBlack()
+                                        self.cardGroup1[self.playerTurn].image = UIImage(named: newCard.0)
+                                        self.cardGroup1[self.playerTurn].isHidden = false
+                                        self.player1.hand.append(newCard.1)
+                                    }
+                                } else if myNumberRed > 0.6 {
+                                    DispatchQueue.main.async {
+                                        let newCard = self.dealRed()
+                                        self.cardGroup1[self.playerTurn].image = UIImage(named: newCard.0)
+                                        self.cardGroup1[self.playerTurn].isHidden = false
+                                        self.player1.hand.append(newCard.1)
+                                    }
+                                }
+                            } else if cardSum1 > 21 {
+                                self.endMatch = true
+                                self.welcomeLabel.text = "Player 2 wins!"
+                                self.welcomeLabel.isHidden = false
+                            }
                         }
-                    } else if mynumberRed>1.0 {
-                        DispatchQueue.main.async {
-                            self.dealRed()
-                            self.turnCounter+=2
-                            if self.turnCounter%2==0
-                            {self.YourturnCounter+=2}                        }
+                        //Safely unwrapping errors
+                        if let myError = error {
+                            print("error", myError)
+                            manager.stopDeviceMotionUpdates()
+                        }
+                        self.welcomeLabel.text = "\(cardSum1)"
+                        self.welcomeLabel.isHidden = false
+                        self.playerTurn += 1
+                    }
+                    else {
+                        // Player 2's turn
+                        self.welcomeLabel.text = "\(self.player2.playerName)'s turn"
+                        self.welcomeLabel.isHidden = false
+                        if let myData = data {
+                            let myNumberBlack = myData.userAcceleration.x
+                            let myNumberRed = myData.userAcceleration.y
+                            for number in self.player2.hand {
+                                if number == 1 {
+                                    cardSum2 += 11
+                                } else if number > 10 {
+                                    cardSum2 += 10
+                                }
+                                else {
+                                    cardSum2 += number
+                                }
+                            }
+                            if cardSum2 < 22 {
+                                if myNumberBlack > 0.6 {
+                                    DispatchQueue.main.async {
+                                        let newCard = self.dealBlack()
+                                        self.cardGroup2[self.playerTurn].image = UIImage(named: newCard.0)
+                                        self.cardGroup2[self.playerTurn].isHidden = false
+                                        self.player2.hand.append(newCard.1)
+                                    }
+                                } else if myNumberRed > 0.6 {
+                                    DispatchQueue.main.async {
+                                        let newCard = self.dealRed()
+                                        self.cardGroup2[self.playerTurn].image = UIImage(named: newCard.0)
+                                        self.cardGroup2[self.playerTurn].isHidden = false
+                                        self.player2.hand.append(newCard.1)
+                                    }
+                                }
+                            } else if cardSum2 > 21 && self.player2.hand.contains(1) {
+                                cardSum2 -= 10
+                                if myNumberBlack > 0.6 {
+                                    DispatchQueue.main.async {
+                                        let newCard = self.dealBlack()
+                                        self.cardGroup2[self.playerTurn].image = UIImage(named: newCard.0)
+                                        self.cardGroup2[self.playerTurn].isHidden = false
+                                        self.player2.hand.append(newCard.1)
+                                    }
+                                } else if myNumberRed > 0.6 {
+                                    DispatchQueue.main.async {
+                                        let newCard = self.dealRed()
+                                        self.cardGroup2[self.playerTurn].image = UIImage(named: newCard.0)
+                                        self.cardGroup2[self.playerTurn].isHidden = false
+                                        self.player2.hand.append(newCard.1)
+                                    }
+                                }
+                            } else if cardSum2 > 21 {
+                                self.endMatch = true
+                                self.welcomeLabel.text = "Player 1 wins!"
+                                self.welcomeLabel.isHidden = false
+                            }
+                        }
+                        //Safely unwrapping errors
+                        if let myError = error {
+                            print("error", myError)
+                            manager.stopDeviceMotionUpdates()
+                        }
+                        self.playerTurn += 1
+                        self.welcomeLabel.text = "\(cardSum2)"
+                    }
+                //Below bracket is endwhile
+                }
+                if let myData = data {
+                    let myNumberBlack = myData.userAcceleration.x
+                    let myNumberRed = myData.userAcceleration.y
+                    if myNumberBlack > 0.6 || myNumberRed > 0.6 {
+                        self.endMatch = false
+                        self.playerTurn = 1
                     }
                 }
-                //Safely unwrapping errors
                 if let myError = error {
-                    print("error",myError)
+                    print("error", myError)
                     manager.stopDeviceMotionUpdates()
                 }
             })
@@ -151,110 +202,45 @@ class ViewController: UIViewController {
         }
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        restartBtn.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 2)
+        upsideDownBtn.transform =
+            CGAffineTransform(rotationAngle: CGFloat.pi)
+        welcomeLabel.text = "Shake me"
+        for card in cardGroup1 {
+            card.isHidden = true
+        }
+        for card in cardGroup2 {
+            card.isHidden = true
+        }
+        updateUI()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    var mypoint:Int=0
-    func dealRed() {
+    
+    func dealRed() -> (String, Int) {
         let random = Int(arc4random_uniform(26)) + 1
-        //add random number to a array
-        randomnum.append(random)
-        print(randomnum)
         let cardName = "red_\(random)"
-        if turnCounter==0{
-            card1.image = UIImage(named: cardName)
-            
-            card1.isHidden = false
-        }
-        if turnCounter==1{
-            card6.image = UIImage(named: cardName)
-            
-            card6.isHidden = false        }
-        if turnCounter==2{
-            card2.image = UIImage(named: cardName)
-            
-            card2.isHidden = false        }
-        if turnCounter==3{
-            card7.image = UIImage(named: cardName)
-            
-            card7.isHidden = false        }
-        if turnCounter==4{
-            card3.image = UIImage(named: cardName)
-            
-            card3.isHidden = false        }
-        if turnCounter==5{
-            card8.image = UIImage(named: cardName)
-            
-            card8.isHidden = false        }
-        if turnCounter==6{
-            card4.image = UIImage(named: cardName)
-            
-            card4.isHidden = false        }
-        if turnCounter==7{
-            card9.image = UIImage(named: cardName)
-            
-            card9.isHidden = false        }
-        if turnCounter==8{
-            card5.image = UIImage(named: cardName)
-            
-            card5.isHidden = false        }
-        if turnCounter==9{
-            card10.image = UIImage(named: cardName)
-            
-            card10.isHidden = false        }
-        welcomeLabel.isHidden = true
-        
+        let cardValue = random % 13
+        return (cardName, cardValue)
     }
-    func dealBlack() {
+    
+    func dealBlack() -> (String, Int) {
         let random = Int(arc4random_uniform(26)) + 1
-        randomnum.append(random)
         let cardName = "black_\(random)"
-        print(turnCounter)
-        print(cardName)
-        if turnCounter==0{
-        card1.image = UIImage(named: cardName)
-        
-        card1.isHidden = false
-        }
-        if turnCounter==1{
-            card6.image = UIImage(named: cardName)
-            
-            card6.isHidden = false        }
-        if turnCounter==2{
-            card2.image = UIImage(named: cardName)
-            
-            card2.isHidden = false        }
-        if turnCounter==3{
-            card7.image = UIImage(named: cardName)
-            
-            card7.isHidden = false        }
-        if turnCounter==4{
-            card3.image = UIImage(named: cardName)
-            
-            card3.isHidden = false        }
-        if turnCounter==5{
-            card8.image = UIImage(named: cardName)
-            
-            card8.isHidden = false        }
-        if turnCounter==6{
-            card4.image = UIImage(named: cardName)
-            
-            card4.isHidden = false        }
-        if turnCounter==7{
-            card9.image = UIImage(named: cardName)
-            
-            card9.isHidden = false        }
-        if turnCounter==8{
-            card5.image = UIImage(named: cardName)
-            
-            card5.isHidden = false        }
-        if turnCounter==9{
-            card10.image = UIImage(named: cardName)
-            
-            card10.isHidden = false        }
-        
-        welcomeLabel.isHidden = true
-        
+        let cardValue = random % 13
+        return (cardName, cardValue)
+    }
+}
+
+class Player {
+    var playerName: String
+    var hand = [Int]()
+    init(name: String) {
+        self.playerName = name
     }
 }
 
